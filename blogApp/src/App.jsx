6839ef212a4fe1d50f626795add907332aa27a2c
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-03-10 21:23:43 +0530

    create the header section

--- blogApp/src/App.jsx
+++ blogApp/src/App.jsx
@@ -5,10 +5,12 @@ import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
 import About from './pages/About';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route element={<Home/>} path='/' />
         <Route element={<Signin/>} path='/sign-in' />
