9fd01e3aeec0b34431f5897698fc396037d75fbd
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-03-13 19:46:33 +0530

    crrate a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req, res) => {
+    res.json({message:"user controller is also working "});
+}
--- api/index.js
+++ api/index.js
@@ -1,16 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+
 
 dotenv.config();
 
 mongoose
-  .connect(
-    process.env.MONGO
-  )
+  .connect(process.env.MONGO)
   .then(() => {
     console.log("mongodb is connected");
-  }).catch((err) =>{
+  })
+  .catch((err) => {
     console.log(err);
   });
 
@@ -19,3 +20,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
+
+app.use("/api/user", userRoutes);
