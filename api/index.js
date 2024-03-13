7580c3ed82fc4861ce35d5c7fbff64f402c7124f
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-03-13 19:17:08 +0530

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => {
+    console.log("mongodb is connected");
+  }).catch((err) =>{
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('server is running on port 3000');
-})
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
