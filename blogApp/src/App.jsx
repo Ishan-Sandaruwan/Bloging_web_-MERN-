7d575692b77efaa417cd9fdfb0b9ab06baef3fb1
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-03-22 07:51:46 +0530

    create the footer component

--- blogApp/src/App.jsx
+++ blogApp/src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/Dashboard';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route element={<About/>} path='/about' />
         <Route element={<Projects/>} path='/projects' />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
