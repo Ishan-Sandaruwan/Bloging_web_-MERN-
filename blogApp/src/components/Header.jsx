9cb29a50295a4c248b75f7b52d98e3da6ff06c64
W I Sandaruwan <sadaruwan0427@gmail.com>
2024-03-29 18:51:47 +0530

    update header component using user data

--- blogApp/src/components/Header.jsx
+++ blogApp/src/components/Header.jsx
@@ -1,58 +1,85 @@
-import React from 'react';
-import { Button, Navbar , TextInput } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
+import React from "react";
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  DropdownDivider,
+  Navbar,
+  TextInput,
+} from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-
-    const path = useLocation().pathname ;
+  const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
 
   return (
-    <Navbar className='border-b-2'>
-      <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Tech</span>
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Tech
+        </span>
         Blog
       </Link>
       <form action="">
-        <TextInput 
-            type='text'
-            placeholder='Search'
-            rightIcon={AiOutlineSearch}
-            className='hidden lg:inline'
+        <TextInput
+          type="text"
+          placeholder="Search"
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-        <AiOutlineSearch/>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
       </Button>
-      <div className='flex gap-2 md:order-2'> 
-        <Button className='w-12 h-10 hidden sm:inline ' color='gray' pill>
-            <FaMoon/>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline " color="gray" pill>
+          <FaMoon />
         </Button>
-        <Link to="sign-in">
-            <Button gradientDuoTone='purpleToBlue' pill outline>
-                Sign In
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" size="sm" rounded img={currentUser.photoURL} />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm cursor-default font-semibold">@{currentUser.username}</span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <DropdownDivider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="sign-in">
+            <Button gradientDuoTone="purpleToBlue" pill outline>
+              Sign In
             </Button>
-        </Link>
+          </Link>
+        )}
+
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path === "/"} as={'div'}>
-            <Link to='/'>
-                Home
-            </Link>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/about"} as={'div'}>
-            <Link to='/about'>
-                About
-            </Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/projects"} as={'div'}>
-            <Link to='/projects'>
-                Projects
-            </Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
