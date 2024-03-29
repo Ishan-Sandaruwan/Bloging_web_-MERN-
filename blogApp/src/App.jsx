1e2d497892371ff3611bb2de06b496a853844271
W I Sandaruwan <sadaruwan0427@gmail.com>
2024-03-29 19:43:42 +0530

    mae dashboard private

--- blogApp/src/App.jsx
+++ blogApp/src/App.jsx
@@ -1,28 +1,31 @@
-import {BrowserRouter, Routes,Route} from 'react-router-dom';
-import Home from './pages/Home';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
-        <Route element={<Home/>} path='/' />
-        <Route element={<Signin/>} path='/sign-in' />
-        <Route element={<Signup/>} path='/sign-up' />
-        <Route element={<Dashboard/>} path='/dashboard' />
-        <Route element={<About/>} path='/about' />
-        <Route element={<Projects/>} path='/projects' />
+        <Route element={<Home />} path="/" />
+        <Route element={<Signin />} path="/sign-in" />
+        <Route element={<Signup />} path="/sign-up" />
+        <Route element={<PrivateRoute />}>
+          <Route element={<Dashboard />} path="/dashboard" />
+        </Route>
+        <Route element={<About />} path="/about" />
+        <Route element={<Projects />} path="/projects" />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ blogApp/src/components/PrivateRoute.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
+
+export default function PrivateRoute() {
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
+}
