ee782ee1f57775e482d269bb1801e3182fe61be3
W I Sandaruwan <sadaruwan0427@gmail.com>
2024-03-29 23:28:17 +0530

    Complete profile page UI

--- blogApp/src/components/DashProfile.jsx
+++ blogApp/src/components/DashProfile.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { FaUserAlt } from "react-icons/fa";
+import { Button, TextInput } from "flowbite-react";
 
 export default function DashProfile() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>DashProfile</div>
-  )
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden">
+          <img
+            src={
+              currentUser.profilePicture ||
+              "https://imgs.search.brave.com/ZZL4lnNTqep3cP88nmZ6YbthIhTsmW5h_tobIuShv8s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWd2/My5mb3Rvci5jb20v/aW1hZ2VzL2Jsb2ct/cmljaHRleHQtaW1h/Z2UvMTAtcHJvZmls/ZS1waWN0dXJlLWlk/ZWFzLXRvLW1ha2Ut/eW91LXN0YW5kLW91/dC5qcGc"
+            }
+            alt="User"
+            className="rounded-full w-full h-full border-8 object-cover border-[lightGray]"
+          />
+        </div>
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.username}
+        />
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+        />
+        <TextInput type="password" id="password" placeholder="**********" />
+        <Button type="submit" outline gradientDuoTone='purpleToBlue'> 
+          Update
+        </Button>
+      </form>
+      <div className="text-red-500 flex justify-between mt-5">
+        <span className="cursor-pointer">Delete account</span>
+        <span className="cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
 }
