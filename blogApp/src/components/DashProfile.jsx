e9fba7636ca2136872b830b1f2033e76e33d0149
W I Sandaruwan <sadaruwan0427@gmail.com>
2024-03-30 08:14:58 +0530

    profile pic changing lvl 1

--- blogApp/src/components/DashProfile.jsx
+++ blogApp/src/components/DashProfile.jsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-import { FaUserAlt } from "react-icons/fa";
 import { Button, TextInput } from "flowbite-react";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async ()=>{
+    console.log('uploading image');
+  };
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden"
+          onClick={() => filePickerRef.current.click()}
+        >
           <img
             src={
+              imageFileUrl ||
               currentUser.profilePicture ||
               "https://imgs.search.brave.com/ZZL4lnNTqep3cP88nmZ6YbthIhTsmW5h_tobIuShv8s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWd2/My5mb3Rvci5jb20v/aW1hZ2VzL2Jsb2ct/cmljaHRleHQtaW1h/Z2UvMTAtcHJvZmls/ZS1waWN0dXJlLWlk/ZWFzLXRvLW1ha2Ut/eW91LXN0YW5kLW91/dC5qcGc"
             }
@@ -32,7 +61,7 @@ export default function DashProfile() {
           defaultValue={currentUser.email}
         />
         <TextInput type="password" id="password" placeholder="**********" />
-        <Button type="submit" outline gradientDuoTone='purpleToBlue'> 
+        <Button type="submit" outline gradientDuoTone="purpleToBlue">
           Update
         </Button>
       </form>
