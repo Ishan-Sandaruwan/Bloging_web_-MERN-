7332e662b333335c6e80946d7dad524b74f0f4e9
W I Sandaruwan <sadaruwan0427@gmail.com>
2024-04-06 22:13:17 +0530

    add update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,45 @@
+import { errorHandler } from "../utils/error";
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
 export const test = (req, res) => {
   res.json({ message: "user controller is also working " });
 };
 
-export const updateUser = async (req, res, next) => {};
+export const updateUser = async (req, res, next) => {
+  if (req.user.id != req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user"));
+  }
+  if (req.body.username) {
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(403, "You are not allowed to update this user"));
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(403, "Username cannot contain spaces"));
+    }
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(403, "Username can only contain letters and numbers")
+      );
+    }
+  }
+  req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { test ,updateUser} from "../controllers/user.controller.js";
+import { test, updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
-router.put("/update:userId", updateUser);
-
+router.put("/update/:userId", verifyToken, updateUser);
 
 export default router;
